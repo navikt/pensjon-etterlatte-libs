111734c17fdd17518d8c3e289c7164344cb6874b
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2023-10-05 08:47:20 +0200

    EY-2830: Går over til TOML for avhengnadar, pluss sett opp dependabot og codescanning (#6)
    * EY-2830: Startar med å oppgradere Gradle, kjens tryggast
    
    * EY-2830: Går over til TOML-basert styring her ogsåß
    
    * EY-2830: Dependabot-konfig
    
    * EY-2830: CodeQL-konfig
    
    * EY-2830: Oppgraderer byggejobbane til å køyre på Java 17. Burde eigentleg gå til 21, men her heng vi så langt etter med mykje at vi startar med 17

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,27 +1,28 @@
 plugins {
     id("maven-publish")
+    id("etterlatte")
 }
 
 dependencies {
     api(kotlin("stdlib"))
     api(kotlin("reflect"))
 
-    api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.4")
-    api("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.13.4")
-    api("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
+    api(libs.jackson.datatypejsr310)
+    api(libs.jackson.datatypejdk8)
+    api(libs.jackson.modulekotlin)
 
-    implementation("io.ktor:ktor-client-core:2.1.1")
-    implementation("io.ktor:ktor-client-logging-jvm:2.1.1")
-    implementation("io.ktor:ktor-client-auth:2.1.1")
-    implementation("io.ktor:ktor-serialization-jackson:2.1.1")
-    implementation("io.ktor:ktor-client-content-negotiation:2.1.1")
+    implementation(libs.ktor2.clientcore)
+    implementation(libs.ktor2.clientloggingjvm)
+    implementation(libs.ktor2.clientauth)
+    implementation(libs.ktor2.jackson)
+    implementation(libs.ktor2.clientcontentnegotiation)
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
-    testImplementation("io.kotest:kotest-assertions-core:5.4.2")
-    testImplementation("io.mockk:mockk:1.13.1")
-    testImplementation("io.ktor:ktor-client-mock:2.1.1")
+    testImplementation(libs.test.jupiter.api)
+    testImplementation(libs.test.jupiter.params)
+    testRuntimeOnly(libs.test.jupiter.engine)
+    testImplementation(libs.test.kotest.assertionscore)
+    testImplementation(libs.test.mockk)
+    testImplementation(libs.ktor2.clientmock)
 }
 
 publishing {
