a87ec0a08b8f9a328e61854aa590182ebe60ecc4
Bjorn Golberg <bjorn.golberg@gmail.com>
2024-08-27 09:41:55 +0200

    Legger til inntektsaar Inntektsjustering

--- common/src/main/kotlin/no/nav/etterlatte/libs/common/inntektsjustering/Inntektsjustering.kt
+++ common/src/main/kotlin/no/nav/etterlatte/libs/common/inntektsjustering/Inntektsjustering.kt
@@ -5,6 +5,7 @@ import java.util.UUID
 
 data class Inntektsjustering(
     val id: UUID,
+    val inntektsaar: Int,
     val arbeidsinntekt: Int,
     val naeringsinntekt: Int,
     val arbeidsinntektUtland: Int,
