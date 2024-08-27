3a6040e2a97cd465cc7bda05af8539ad4b1a8b4a
Bjorn Golberg <bjorn.golberg@gmail.com>
2024-08-27 09:39:46 +0200

    Fjerner felter

--- common/src/main/kotlin/no/nav/etterlatte/libs/common/inntektsjustering/Inntektsjustering.kt
+++ common/src/main/kotlin/no/nav/etterlatte/libs/common/inntektsjustering/Inntektsjustering.kt
@@ -1,14 +1,10 @@
 package no.nav.etterlatte.libs.common.inntektsjustering
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import java.time.Instant
 import java.util.UUID
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 data class Inntektsjustering(
     val id: UUID,
-    val sakId: Long,
-    val aar: Int,
     val arbeidsinntekt: Int,
     val naeringsinntekt: Int,
     val arbeidsinntektUtland: Int,
